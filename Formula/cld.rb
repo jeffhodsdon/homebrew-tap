fda20a94052a052a9df52e0abb161fb044419c57
Jeff Hodsdon <jeffhodsdon@gmail.com>
2026-02-23 09:33:25 +0100

    Initial formula for cld with service support

---
+++ Formula/cld.rb
@@ -0,0 +1,51 @@
+class Cld < Formula
+  desc "iMessage + Claude CLI personal assistant daemon"
+  homepage "https://github.com/jeffhodsdon/cld"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/jeffhodsdon/cld/releases/download/latest/cld-aarch64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+
+    on_intel do
+      url "https://github.com/jeffhodsdon/cld/releases/download/latest/cld-x86_64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  def install
+    bin.install "cld"
+  end
+
+  service do
+    run opt_bin/"cld"
+    keep_alive true
+    process_type :background
+    log_path var/"log/cld.log"
+    error_log_path var/"log/cld.err.log"
+    working_dir var/"cld"
+    environment_variables HOME: Dir.home, PATH: std_service_path_env
+    restart_delay 10
+  end
+
+  def caveats
+    <<~EOS
+      To start cld as a background service:
+        brew services start cld
+
+      After upgrading:
+        brew services restart cld
+
+      Logs are at:
+        #{var}/log/cld.log
+        #{var}/log/cld.err.log
+    EOS
+  end
+
+  test do
+    assert_match "cld", shell_output("#{bin}/cld --version")
+  end
+end
